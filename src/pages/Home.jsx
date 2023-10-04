fee173fccde8bd93503d00c8d549e612b1ad8d75
Bertrand Sudre <sudrebertrand@gmail.com>
2023-10-04 15:30:13 +0200

    mockdata

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,40 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import useFetchData from '../hooks/useFetchData';
-import Header from '../layout/Header';
-import VerticalNav from '../components/VerticalNav';
-import Analytics from '../components/Analytics';
-import Hero from '../components/Hero';
-import LoadingSpinner from '../components/LoadingSpinner';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetchData from "../hooks/useFetchData";
+import Header from "../layout/Header";
+import VerticalNav from "../components/VerticalNav";
+import Analytics from "../components/Analytics";
+import Hero from "../components/Hero";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const Home = () => {
-    const { userId } = useParams();
-    const { data, loading, error } = useFetchData(userId, false);
+  const { userId } = useParams();
+  const { data, loading, error } = useFetchData(userId, true);
 
-    return (
-        <>
-            <VerticalNav />
-            <Header userId={userId} />
-            <main>
-                {loading ? (
-                    <LoadingSpinner />
-                ) : error ? (
-                    <section className="error">
-                        Erreur : {error.message}
-                    </section>
-                ) : (
-                    data.user &&
-                    data.activity &&
-                    data.averageSessions &&
-                    data.performance && (
-                        <>
-                            <Hero user={data.user} />
+  return (
+    <>
+      <VerticalNav />
+      <Header userId={userId} />
+      <main>
+        {loading ? (
+          <LoadingSpinner />
+        ) : error ? (
+          <section className="error">Erreur : {error.message}</section>
+        ) : (
+          data.user &&
+          data.activity &&
+          data.averageSessions &&
+          data.performance && (
+            <>
+              <Hero user={data.user} />
 
-                            <Analytics data={data} />
-                        </>
-                    )
-                )}
-            </main>
-        </>
-    );
+              <Analytics data={data} />
+            </>
+          )
+        )}
+      </main>
+    </>
+  );
 };
 
 export default Home;
